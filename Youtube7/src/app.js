27745f961dfceb2d9a5ee255c1a1907d65a515f5
Hasmat <mdhasmat2023@gmail.com>
2025-09-30 22:13:33 +0530

    ROUTES added

--- Youtube7/src/app.js
+++ Youtube7/src/app.js
@@ -20,6 +20,7 @@ import userRouter from "./routes/user.routes.js"
 import healthcheckRouter from "./routes/healthcheck.routes.js"
 import tweetRouter from "./routes/tweet.routes.js"
 import commentRouter from "./routes/comment.routes.js"
+import playlistRouter from "./routes/playlist.routes.js"
 
 
 //routes declaration 
@@ -27,6 +28,7 @@ app.use("/api/v1/healthcheck", healthcheckRouter)
 app.use("/api/v1/users", userRouter)
 app.use("/api/v1/tweets", tweetRouter)
 app.use("/api/v1/comment", commentRouter)
+app.use("/api/v1/playlist", playlistRouter)
 
 //http://localhost:8000/api/v1/users/register
 
